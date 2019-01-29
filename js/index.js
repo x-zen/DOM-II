b9bd46335eadd4a4635723b28c720eef9d7fb778
x-zen <jdharris.x@gmail.com>
2019-01-29 17:21:44 -0600

    # Help

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+const btnAttr = document.querySelector('.btn');
+btnAttr[0].addEventListener('click', e => {
+  console.log('stuff works now');
+  e.stopPropagation();
+});
+
+/*
+navTxt[i].forEach(link => {
+  link.addEventListener;
+});
+
+navTxt[0].addEventListener('mouseover', e =>  {
+  e.stopPropagation();
+  TweenMax.to(e.currentTarget, 2, {
+    textDecoration: 'lineThrough'
+  });
+});
+*/
